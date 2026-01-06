ce3bead779aae9c84e17d59acb5d573f6d61798a
SWillames <sergiowoc@gmail.com>
2026-01-06 01:42:14 -0300

    criando o projeto.

---
+++ src/test/java/br/com/swsoftware/RangoApp/RangoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.swsoftware.RangoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
