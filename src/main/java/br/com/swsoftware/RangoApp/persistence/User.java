dca830b347df96e73110dd04885151009cc85220
SWillames <sergiowoc@gmail.com>
2026-01-15 17:32:38 -0300

    feat: entidade usuario adicionada.

---
+++ src/main/java/br/com/swsoftware/RangoApp/persistence/User.java
@@ -0,0 +1,43 @@
+package br.com.swsoftware.RangoApp.persistence;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false,  unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
