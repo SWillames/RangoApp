48de97947034cf21ef2a3409b70bc677384d0aec
SWillames <sergiowoc@gmail.com>
2026-01-19 17:54:45 -0300

    feat: adicionando os objetos de transferencia de dados(DTO)

---
+++ src/main/java/br/com/swsoftware/rangoapp/application/dto/UserPasswordDTO.java
@@ -0,0 +1,13 @@
+package br.com.swsoftware.rangoapp.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserPasswordDTO {
+
+    private String newPassword;
+}
---
+++ src/main/java/br/com/swsoftware/rangoapp/application/dto/UserResponseDTO.java
@@ -0,0 +1,23 @@
+package br.com.swsoftware.rangoapp.application.dto;
+
+import br.com.swsoftware.rangoapp.persistence.UserType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String login;
+    private String address;
+    private UserType type;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdate;
+}
